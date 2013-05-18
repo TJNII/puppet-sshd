c3388531dd81bed29f5868f042c581d4c38647a2
root <root@puppetmaster.tjnii.com>
2013-05-17 21:19:43 -0600

    Initial import

---
+++ manifests/init.pp
@@ -0,0 +1,27 @@
+class sshd {
+  case $operatingsystem {
+    centos, redhat: {
+      $service_name = 'sshd'
+      $os_file = 'cent'
+    }
+    debian, ubuntu: {
+      $service_name = 'ssh'
+      $os_file = 'debian'
+    }
+  }
+                                    
+  
+   file { '/etc/ssh/sshd_config':
+      ensure => file,
+      mode   => 600,
+      source => "puppet:///modules/sshd/sshd_config.$os_file",
+   }
+   
+   service { 'ssh':
+     name       => $service_name,
+     ensure     => running,
+     enable     => true,
+     subscribe  => File['/etc/ssh/sshd_config'],
+   }
+   
+}
