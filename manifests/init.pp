7bfc967861dc855177625ceeab9ae2b8d006e310
Tom Noonan II <tom@tjnii.com>
2013-09-22 19:32:39 -0500

    Changed configs to templates and made listen port an argument

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,27 +1,30 @@
-class sshd {
-  case $operatingsystem {
-    centos, redhat: {
-      $service_name = 'sshd'
-      $os_file = 'cent'
+class sshd (
+  $listen_port = 22,
+  ) {
+    case $operatingsystem {
+      centos, redhat: {
+        $service_name = 'sshd'
+        $os_file = 'cent'
+      }
+      debian, ubuntu: {
+        $service_name = 'ssh'
+        $os_file = 'debian'
+      }
     }
-    debian, ubuntu: {
-      $service_name = 'ssh'
-      $os_file = 'debian'
-    }
-  }
-                                    
-  
-   file { '/etc/ssh/sshd_config':
+    
+    
+    file { '/etc/ssh/sshd_config':
       ensure => file,
       mode   => 600,
-      source => "puppet:///modules/sshd/sshd_config.$os_file",
-   }
-   
-   service { 'ssh':
-     name       => $service_name,
-     ensure     => running,
-     enable     => true,
-     subscribe  => File['/etc/ssh/sshd_config'],
-   }
-   
-}
+      content => template("sshd/sshd_config.${os_file}.erb"),
+#      source => "puppet:///modules/sshd/sshd_config.$os_file",
+    }
+    
+    service { 'ssh':
+      name       => $service_name,
+      ensure     => running,
+      enable     => true,
+      subscribe  => File['/etc/ssh/sshd_config'],
+    }
+  }
+  
